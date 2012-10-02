0fc3c80536bbd95d0802278c1ed2c8f3402ef714
michaelqtlam@gmail.com <michaelqtlam@gmail.com@ef141d2e-0c7f-f45c-56c5-d3e54007eb92>
2012-10-02 22:24:35 +0000

    Implemented ModelBasedAgent

--- trunk/project1/ModelBasedAgent.py
+++ trunk/project1/ModelBasedAgent.py
@@ -3,6 +3,7 @@ from Agent import *
 
 class ModelBasedAgent(Agent):
 	"""Deterministic model-based reflex agent with a small amount of memory."""
+	# 3 bits = 2^3 = up to 8 states
 	state = 0
 
 	def __init__(self):
@@ -10,5 +11,43 @@ class ModelBasedAgent(Agent):
 	
 	def takeStep(self, percept):
 		[wall, dirt, home] = percept
-		return Agent.OFF
+		if self.state == 0 and dirt == 1:
+			return Agent.SUCK
+		if self.state == 0 and wall == 0:
+			return Agent.FORWARD
+		if self.state == 0 and wall == 1:
+			self.state = 1
+			return Agent.RIGHT
+		if self.state == 1 and wall == 1:
+			self.state = 6
+			return Agent.RIGHT 
+		if self.state == 1 and wall == 0:
+			self.state = 2
+			return Agent.FORWARD
+		if self.state == 2:
+			self.state = 3
+			return Agent.RIGHT
+		if self.state == 3 and dirt == 1:
+			return Agent.SUCK
+		if self.state == 3 and wall == 0:
+			return Agent.FORWARD
+		if self.state == 3 and wall == 1:
+			self.state = 4
+			return Agent.LEFT
+		if self.state == 4 and wall == 1:
+			self.state = 6
+			return Agent.RIGHT
+		if self.state == 4 and wall == 0:
+			self.state = 5
+			return Agent.FORWARD
+		if self.state == 5:
+			self.state = 0
+			return Agent.LEFT
+		if self.state == 6 and home == 1:
+			return Agent.OFF
+		if self.state == 6 and wall == 0:
+			return Agent.FORWARD
+		if self.state == 6 and wall == 1:
+			return Agent.RIGHT
+		raise NotImplementedError("Unreachable statement.")
 
