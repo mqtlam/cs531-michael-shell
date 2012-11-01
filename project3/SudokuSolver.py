9fd518f79273f8d576e43299012e470cd953fb7a
michaelqtlam@gmail.com <michaelqtlam@gmail.com@ef141d2e-0c7f-f45c-56c5-d3e54007eb92>
2012-11-01 07:22:47 +0000

    Implemented backtracking search and refined constraint propagation. TODO: rule 2 of constraint propagation and naked triples.

--- project3/SudokuSolver.py
+++ project3/SudokuSolver.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 from time import clock, time
+from random import choice
 
 class SudokuSolver:
 	"""The Sudoku class represents a sudoku board."""
 
-	def __init__(self):
+	def __init__(self, useRandomUnassignedVariable = False):
 		"""Initialize the Sudoku board and CSP formulation."""
 
 		### Sudoku board construction
@@ -28,14 +29,17 @@ class SudokuSolver:
 		# maps each square to a set of squares affected by it (should have 20 affected squares per square)
 		self.neighbors = dict((s, list(set(sum([i for i in self.alldiffs if s in i],[]))-set([s]))) for s in self.squares)
 
+		### Options
+		self.useRandomUnassignedVariable = useRandomUnassignedVariable
+
 		### Variables to keep track of statistics
-		self.numBacktracking = 0
+		self.numBacktrackings = 0
 		self.solved = None
 
 	def setup(self, board):
 		"""Initializes the board configuration to board."""
 		# reset all variables
-		self.__init__()
+		self.variables = dict((s, self.domain) for s in self.squares)
 
 		# set up board
 		for i, digit in enumerate(board):
@@ -49,23 +53,23 @@ class SudokuSolver:
 			return self.solved
 
 		startClock = time()
-		self.constraintPropagation()
+		self.variables = self.backtrackSearch(self.variables)
 		endClock = time()
 
-		# [success?, numBacktracking, time to solve] # TODO: return more statistics
-		self.solved = [self.isSolved(), 0, endClock - startClock]
+		# [success?, numBacktrackings, time to solve] # TODO: return more statistics
+		self.solved = [self.isSolved(self.variables), self.numBacktrackings, endClock - startClock]
 		return self.solved 
 
-	def isSolved(self):
+	def isSolved(self, variables):
 		"""Returns true if the current board is solved."""
 		for square in self.neighbors:
-			if len(self.variables[square]) != 1:
+			if len(variables[square]) != 1:
 				return False
-			for neighbor in self.neighbors[square]:
-				if len(self.variables[neighbor]) != 1:
+			"""for neighbor in self.neighbors[square]:
+				if len(variables[neighbor]) != 1:
 					return False
-				elif self.variables[neighbor] == self.variables[square]:
-					return False
+				elif variables[neighbor] == variables[square]:
+					return False"""
 		return True
 
 	def printBoard(self):
@@ -89,18 +93,94 @@ class SudokuSolver:
 
 	### CSP Functions
 
-	def constraintPropagation(self):
+	def constraintPropagation(self, variables):
+		"""Perform constraint propagation by assigning consistent values. 
+		Return updated domains for variables or False upon contradiction."""
 		changed = True
 		while changed:
 			changed = False
-			for square in self.variables:
-				value = self.variables[square]
-				if len(value) == 1:
+			for square in variables:
+				values = variables[square]
+				# if no candidates left, contradiction
+				if len(values) == 0:
+					return False
+
+				# (Rule 1) if exactly one candidate, make assignment and propagate
+				elif len(values) == 1:
 					for neighbor in self.neighbors[square]:
-						if value in self.variables[neighbor]:
-							self.variables[neighbor] = self.variables[neighbor].replace(value,'')
+						if values in variables[neighbor]:
+							variables[neighbor] = variables[neighbor].replace(values,'')
 							changed = True
-	
-	# TODO
-	def search(self):
+				"""elif len(values) > 1:
+					candidates = values
+					for v in candidates:
+						found = False
+						for neighbor in self.neighbors[square]:
+							if v in variables[neighbor]:
+								found = True
+								break
+						if found is False:
+							if v not in variables[neighbor]:
+								break
+							self.printFullBoard()
+							variables[neighbor] = variables[neighbor].replace(v, '')
+							changed = True
+							break"""
+		return variables
+
+	def nakedK(self, k, variables):
+		"""Performs the "naked triples" strategy for constraint propagation for a general k."""
 		pass
+
+	def backtrackSearch(self, variables):
+		"""Perform backtracking search by trying assignments systematically. 
+		Recursive function returns completed board upon success or False (backtracking) 
+		upon reaching a contradiction."""
+		# if assignment is complete
+		if self.isSolved(variables):
+			return variables
+
+		# select a variable to make assignment
+		square = self.selectUnassignedVariable(variables)
+		for value in self.orderDomainValues(square, variables):
+			# make an assignment and perform constraint propagation
+			newVariables = variables.copy()
+			newVariables[square] = value
+			newVariables = self.constraintPropagation(newVariables)
+
+			# keep searching until no candidates left for some cell or entire board is solved
+			if newVariables is not False:
+				result = self.backtrackSearch(newVariables)
+				if result is not False:
+					return result
+		# backtrack
+		self.numBacktrackings += 1
+		return False
+
+	def selectUnassignedVariable(self, variables):
+		"""Select an unassigned variable (square) to try an assignment. 
+		Helper method to switch among different heuristics."""
+		if self.useRandomUnassignedVariable is True:
+			return self.selectRandomVariable(variables)
+		else:	
+			return self.selectMostConstrainedVariable(variables)
+
+	def selectMostConstrainedVariable(self, variables):
+		"""Select the least constraining unassigned variable to try an assignment."""
+		minLength = 10
+		minSquare = ""
+		for square in variables:
+			length = len(variables[square])
+			if length < minLength and length > 1:
+				minSquare = square
+				minLength = length
+		return minSquare
+
+	def selectRandomVariable(self, variables):
+		"""Select a random unassigned variable to try an assignment."""
+		squaresRemaining = [square for square in variables if len(variables[square]) > 1]
+		return choice(squaresRemaining)
+
+	def orderDomainValues(self, var, variables):
+		"""Order the domain values."""
+		return variables[var]
