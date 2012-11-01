f72d878424c6a978415c2480649b9dff4d269dd4
michaelqtlam@gmail.com <michaelqtlam@gmail.com@ef141d2e-0c7f-f45c-56c5-d3e54007eb92>
2012-11-01 08:30:42 +0000

    Implemented Rule Two. Remaining TODO is naked triples.

--- trunk/project3/SudokuSolver.py
+++ trunk/project3/SudokuSolver.py
@@ -38,8 +38,9 @@ class SudokuSolver:
 
 		### Variables to keep track of statistics
 		self.numBacktrackings = 0
+		self.numRuleTwo = 0
+		self.numFilledIn = 0
 		self.solved = None
-
 
 	def setup(self, board):
 		"""Initializes the board configuration to board."""
@@ -51,6 +52,7 @@ class SudokuSolver:
 			index = '' + self.rows[i/9] + self.cols[i%9]
 			if digit != "0":
 				self.variables[index] = digit
+				self.numFilledIn += 1
 
 	def solve(self):
 		"""Attempts to solve the current board."""
@@ -61,13 +63,12 @@ class SudokuSolver:
 		self.variables = self.backtrackSearch(self.variables)
 		endClock = time()
 
-		# [success?, numBacktrackings, time to solve] # TODO: return more statistics
-		self.solved = [self.isSolved(self.variables), self.numBacktrackings, endClock - startClock]
+		self.solved = [self.isSolved(self.variables), self.numBacktrackings, self.numRuleTwo, self.numFilledIn, endClock - startClock]
 		return self.solved 
 
 	def isSolved(self, variables):
 		"""Returns true if the current board is solved."""
-		for square in self.neighbors:
+		for square in self.variables:
 			if len(variables[square]) != 1:
 				return False
 			"""for neighbor in self.neighbors[square]:
@@ -77,21 +78,27 @@ class SudokuSolver:
 					return False"""
 		return True
 
-	def printBoard(self):
+	def printBoard(self, variables = None):
 		"""Prints the current board. For debugging purposes."""
+		if variables is None:
+			variables = self.variables
+
 		print "  a b c   d e f   g h i"
 		for r in self.rows:
-			print r, ''.join((self.variables[r+c] if len(self.variables[r+c]) == 1 else '-')+' '+('| ' if c in 'cf' else '') for c in self.cols)
+			print r, ''.join((variables[r+c] if len(variables[r+c]) == 1 else '-')+' '+('| ' if c in 'cf' else '') for c in self.cols)
 			if r in '36':
 				print ' ' + '-'*23
 		print
 
-	def printFullBoard(self):
+	def printFullBoard(self, variables = None):
 		"""Prints the current board, complete with the domain for each variable. For debugging purposes."""
-		width = 1+max(len(self.variables[s]) for s in self.squares)
+		if variables is None:
+			variables = self.variables
+		
+		width = 1+max(len(variables[s]) for s in self.squares)
 		print '  ' + ''.join(c.center(width)+('  ' if c in 'cf' else '') for c in "abcdefghi")
 		for r in self.rows:
-			print r, ''.join(self.variables[r+c].center(width)+('| ' if c in 'cf' else '') for c in self.cols)
+			print r, ''.join(variables[r+c].center(width)+('| ' if c in 'cf' else '') for c in self.cols)
 			if r in '36':
 				print ' -' + '-'.join(['-'*(width*3)]*3)
 		print
@@ -104,43 +111,62 @@ class SudokuSolver:
 		changed = True
 		while changed:
 			changed = False
+
+			# (rule 1) - implicit in representation
+
+			# (rule 2)
+			(variables, changed) = self.ruleTwo(variables, changed)
+
+			# (rule 3) - naked triples
+			(variables, changed) = self.nakedK(2, variables, changed)
+			(variables, changed) = self.nakedK(3, variables, changed)
+
+			# propagation step
 			for square in variables:
 				values = variables[square]
-				# if no candidates left, contradiction
+				# if no candidates left, then contradiction
 				if len(values) == 0:
 					return False
 
-				# (Rule 1) if exactly one candidate, make assignment and propagate
+				# if exactly one candidate, then propagate
 				elif len(values) == 1:
 					for neighbor in self.neighbors[square]:
 						if values in variables[neighbor]:
 							variables[neighbor] = variables[neighbor].replace(values,'')
 							changed = True
-				"""elif len(values) > 1:
-					candidates = values
-					for v in candidates:
-						found = False
-						for neighbor in self.neighbors[square]:
-							if v in variables[neighbor]:
-								found = True
-								break
-						if found is False:
-							if v not in variables[neighbor]:
-								break
-							self.printFullBoard()
-							variables[neighbor] = variables[neighbor].replace(v, '')
-							changed = True
-							break"""
 		return variables
 
-	def nakedK(self, k, variables):
+	def ruleTwo(self, variables, changed):
+		"""Rule two for constraint propagation: assign to any cell a value x 
+		if x is not in the domain of any other cell in that row, column, or box."""
+		for square in variables:
+			candidates = variables[square]
+			if len(candidates) < 2:
+				break
+			for value in candidates:
+				found = False
+				for neighbor in self.neighbors[square]:
+					if value in variables[neighbor]:
+						found = True
+						break
+				if found is False:
+					if value not in variables[square]:
+						assert(False)
+					variables[square] = value
+					self.numRuleTwo += 1
+					changed = True
+					break
+		return [variables, changed]
+
+	def nakedK(self, k, variables, changed):
 		"""Performs the "naked triples" strategy for constraint propagation for a general k."""
-		pass
+		# TODO
+		return [variables, changed]
 
 	def backtrackSearch(self, variables):
 		"""Perform backtracking search by trying assignments systematically. 
 		Recursive function returns completed board upon success or False (backtracking) 
-		upon reaching a contradiction."""
+		upon reaching a contradiction. Based on textbook."""
 		# if assignment is complete
 		if self.isSolved(variables):
 			return variables
