30fbfbfbc40cbe29184f7cf8c29ee01c92736ca8
michaelqtlam@gmail.com <michaelqtlam@gmail.com@ef141d2e-0c7f-f45c-56c5-d3e54007eb92>
2012-11-02 22:38:13 +0000

    Added the optional command line arguments for using random slot and naked triple rules. Also, naked triple rules off by default now.

--- project3/runSudoku.py
+++ project3/runSudoku.py
@@ -4,14 +4,18 @@ import sys
 from time import clock, time
 
 def printUsage():
-	print "Usage: " + sys.argv[0] + " [-i sequence|-f inputFile]"
-	print "\tChoose one run option:"
+	print "Usage: " + sys.argv[0] + " (-i sequence|-f inputFile) [--random|--naked K]"
+	print "\n\tChoose one run option:"
 	print "\t" + sys.argv[0] + " -i \"sequence\""
 	print "\t\treads in a board sequence string"
 	print "\t\te.g. \"240300000000520407000046008610700084009060500730005061100470000302051000000002019\""
 	print "\t" + sys.argv[0] + " -f inputFile"
 	print "\t\treads from input file"
 	print "\t\tmust be formatted like repository.txt"
+	print "\n\tOptional Arguments (must be specified after run option above):"
+	print "\t\t--random: picks random slot instead of most constrained slot for unassigned variable heuristic"
+	print "\t\t--naked K: enables naked-K rule, where K is a string of integers 1-9,"
+	print "\t\t\te.g. --naked 23 enables naked doubles (K=2) and naked triples rules (K=3)."
 
 def validBoardSeq(seq):
 	"""Checks if the board sequence is valid."""
@@ -60,9 +64,25 @@ else:
 	printUsage()
 	exit(3)
 
-### Set up sudoku solver
+# Optional Arguments
 useRandomUnassignedVariable = False
-useNakedStrategy = [2, 3]
+useNakedStrategy = []
+
+if len(sys.argv) > 3:
+	i = 3
+	while i < len(sys.argv):
+		if sys.argv[i] == "--random":
+			useRandomUnassignedVariable = True
+		if sys.argv[i] == "--naked":
+			if i+1 < len(sys.argv):
+				useNakedStrategy = [int(x) for x in set(sys.argv[i+1])]
+				print useNakedStrategy
+			else:
+				printUsage()
+				exit(4)
+		i+=1
+
+### Set up sudoku solver
 sudoku = SudokuSolver.SudokuSolver(useRandomUnassignedVariable, useNakedStrategy)
 for problem, comment in zip(problemSet, problemComments):
 	print "--------------------------------------------------"
