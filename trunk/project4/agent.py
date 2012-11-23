dcc27903bad48c563e7cf3f6bca2c212f41ded2a
michaelqtlam@gmail.com <michaelqtlam@gmail.com@ef141d2e-0c7f-f45c-56c5-d3e54007eb92>
2012-11-23 20:41:12 +0000

    Implemented the algorithm in the book for agent. Has no A* search yet, but template is there. It runs really slowly and it may still have some bugs. Will try to optimize and make better.

--- trunk/project4/agent.py
+++ trunk/project4/agent.py
@@ -2,6 +2,11 @@ import random
 import env
 import KnowledgeBase
 
+NORTH = 0
+EAST = 1
+SOUTH = 2
+WEST = 3
+
 class LogicAgent(object):
 	"""
 	A hybrid agent that uses logic and a planner to work its way through the
@@ -14,14 +19,137 @@ class LogicAgent(object):
 		"""
 		self.logic = None
 		self.environ = None
-		self.actions = 0
+		self.timer = 0
 		self.hasArrow = True
 		self.KB = None
 		
 	def search(self, environment, logicEngine): #TODO TODO TODO
 		"""
-		Executes a logical search for the gold
+		Runs the program loop.
 		"""
+		self.environ = environment
+		self.logic = logicEngine
+		self.hasArrow = True
+		success = False
+		dead = False
+		haveGold = False
+		x, y = (1,1)
+		facing = NORTH
+
+		# persistent agent variables
+		self.KB = KnowledgeBase.KnowledgeBase(self.logic)
+		self.KB.initWumpusWorldLogic()
+		self.timer = 0 # timer
+		self.plan = [] # action sequence
+
+		# "hack" to include these percepts for environment
+		scream = False
+		bump = False
+
+		while not success and not dead:
+			# get percepts
+			(breeze, stench, glitter) = self.environ.sense(x,y)
+
+			dead = self.environ.isDeadly(x,y) 
+
+			if not dead:
+				action = self.hybridWumpusAgent([glitter, stench, breeze, bump, scream], (x, y))
+
+				if bump:
+					bump = False
+				if scream:
+					scream = False
+
+				if action == "Forward":
+					if facing == NORTH:
+						bump = self.environ.canMove(x, y+1)
+						if not bump:
+							(x, y) = (x, y+1)
+					if facing == EAST:
+						bump = self.environ.canMove(x+1, y)
+						if not bump:
+							(x, y) = (x+1, y)
+					if facing == SOUTH:
+						bump = self.environ.canMove(x, y-1)
+						if not bump:
+							(x, y) = (x, y-1)
+					if facing == WEST:
+						bump = self.environ.canMove(x-1, y)
+						if not bump:
+							(x, y) = (x-1, y)
+				elif action == "TurnLeft":
+					facing = facing - 1 % 4 
+				elif action == "TurnRight":
+					facing = facing + 1 % 4 
+				elif action == "Shoot" and self.hasArrow:
+					wumpusHit = self.environ.shootArrow(x, y, facing)	
+					self.hasArrow = False
+					if wumpusHit:
+						scream = True
+				elif action == "Grab":
+					if glitter and not haveGold:
+						haveGold = True	
+				elif action == "Climb":
+					if (x, y) == (0, 0):
+						success = True
+					#else:
+					#	break
+
+		return (success, dead, self.timer, self.hasArrow)
+
+	### Wumpus Hybrid Algorithm ###
+
+	def hybridWumpusAgent(self, percept, current):
+		"""
+		Main logic algorithm.
+		percept is a list of True/False: [glitter, stench, breeze, bump, scream]
+		"""
+		self.KB.tell(self.KB.makePerceptStatement(percept, self.timer))
+		
+		# tell KB temporal physics sentences for time t
+
+		safe = [(x,y) for x in range(0, self.environ.size) for y in range(0, self.environ.size) if self.KB.ask("Safe(Pos("+str(x)+","+str(y)+"))")]
+		
+		unvisited = [] 
+		if self.KB.ask("GlitterAt("+str(self.timer)+")"):
+			self.plan = ["Grab"] + self.planRoute(current, [(0,0)], safe) + ["Climb"]
+		if self.plan == []:
+			unvisited = [(x,y) for x in range(0, self.environ.size) for y in range(0, self.environ.size) if all(self.KB.ask("Location(Pos("+str(x)+","+str(y)+"),"+str(t)+")") for t in range(0, self.timer))]
+			self.plan = self.planRoute(current, list(set(unvisited).intersection(set(safe))), safe)
+		if self.plan == [] and self.ask("HaveArrow("+str(self.timer)+")"):
+			possibleWumpus = [(x,y) for x in range(0, self.environ.size) for y in range(0, self.environ.size) if not self.KB.ask("-Wumpus(Pos("+str(x)+","+str(y)+"))")]
+			self.plan = self.planRoute(current, possibleWumpus, safe)
+		if self.plan == []: # no choice, but to take a risk
+			notUnsafe = [(x,y) for x in range(0, self.environ.size) for y in range(0, self.environ.size) if not self.KB.ask("-OK(Pos("+str(x)+","+str(y)+",),"+str(t)+")")]
+			self.plan = self.planRoute(current, list(set(unvisited).intersection(set(notUnsafe))), safe)
+		if self.plan == []:
+			self.plan = self.planRoute(current, [(0,0)], safe) + ["Climb"]
+		action = self.plan.pop(0)
+
+		self.KB.tell(self.KB.makeActionStatement(action, self.timer))
+		self.timer += 1
+
+		return action
+
+	def planRoute(self, current, goals, allowed):
+		"""
+		Plan a route. Calls A*.
+		current: current location (x,y)
+		goals: 	 list of goal locations [(x1,y1),...,(xn,yn)]
+		allowed: list of squares allowed to go
+		"""
+		actionSequence = []
+		# TODO
+		# dumb random actions for now 
+		actionSequence = [random.choice(["Forward", "TurnLeft", "TurnRight", "Shoot", "Grab","Glimb"])]
+		return actionSequence
+
+	### End Wumpus Hybrid Algorithm ###
+
+	"""def search(self, environment, logicEngine): #TODO TODO TODO
+		""
+		Executes a logical search for the gold
+		""
 		self.environ = environment
 		self.logic = logicEngine
 		self.actions = 0
@@ -64,5 +192,4 @@ class LogicAgent(object):
 				self.KB.tell(self.KB.makeActionStatement("Forward", self.actions))
 				self.actions += 1
 
-		return (success, dead, self.actions, self.hasArrow)
-
+		return (success, dead, self.actions, self.hasArrow)"""
