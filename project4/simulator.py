05bd6119860514694051a4a5035c1f2886a446dd
dom343@gmail.com <dom343@gmail.com@ef141d2e-0c7f-f45c-56c5-d3e54007eb92>
2012-11-29 06:50:02 +0000

    experiments

--- project4/simulator.py
+++ project4/simulator.py
@@ -8,31 +8,38 @@ import logic
 #---------------------------------------MAIN--------------------------------------
 if __name__ == "__main__":
 
-	directory = "./test_maps/"
+	directory = "./maps/"
 
 	#create the agent
 	player = agent.LogicAgent()
 
 	print "file name, is solveable?, has good start?, map size, success?, died?, num actions, has arrow"
 
+    	fp = open("log.txt", "w")
+
 	#for each of the map files run a simulation
-	for aFile in os.listdir(directory):
+	for ind,aFile in enumerate(os.listdir(directory)):
 
 		if aFile == ".svn":
 			continue
 
+        	print '********************************* %d ***********************************' % ind
+
 		#build the environment
 		theMap = env.Environment(os.path.join(directory,aFile))
-	
+
 		#create the logic engine
 		kb = logic.FluentLogic()
-	
+
 		#run the agent and get the results
-		(success, died, actions, hasArrow) = player.search(theMap, kb)
+		(success, died, actions, hasArrow, rewards, steps, gold) = player.search(theMap, kb)
+        	fp.write("[%d] success = %d, gold = %d, kill_wumpus = %d, steps = %d, rewards = %d\n" % (ind,success,gold,hasArrow,steps,rewards))
 
 		#map, possible, can start?, size, success, died?, error?, actions, arrow
 		print "%s, %s, %s, %i, %s, %s, %i, %s" %\
 		(os.path.basename(theMap.fileName), theMap.isSolveable(),\
 		not theMap.noLogicalStart(), theMap.size, success, died,\
 		actions, hasArrow)
-		
+
+    	fp.close()
+
